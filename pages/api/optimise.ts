608e70c2daf67b3bb77346e6a554d46ed82da7e7
Anurag30112003 <anuragsharma011011@gmail.com>
2023-01-28 17:12:18 +0530

    new feature optimise

--- pages/api/optimise.ts
+++ pages/api/optimise.ts
@@ -3,15 +3,15 @@ import openai from "../../util/openai";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
 
-const { database , tablename , fields , query } : {database:string,tablename:string,fields:string,query:string} = req.body;
+const { query } : {query:string} = req.body;
 
 const response = await openai.createCompletion({
   model: "code-davinci-002",
-  prompt: `### ${database} SQL tables, with their properties:\n#\n# ${tablename} (${fields})\n  \n#\n### ${query} \nSELECT`,
+  prompt: `###  \n#\n### ${query} optimise this query to make it more fast and short. \nSELECT `,
   temperature: 0,
-  max_tokens: 150,
+  max_tokens: 150,  
   top_p: 1,
-  frequency_penalty: 0,
+  frequency_penalty: 0, 
   presence_penalty: 0,
   stop: ["#", ";"],
 });
